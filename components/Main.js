a5bafae87c6fbb569887dc100e1fe4c525bfc2d1
Ipsita07p <ipsitapati.0708@gmail.com>
2023-11-15 23:11:34 +0530

    learniing

--- components/Main.js
+++ components/Main.js
@@ -1,25 +1,16 @@
-/**
-Challenge: Build the main section!
-
-Skip 2 aspects of the design for now:
-1. The colored bullets in the list
-2. The larger React logo on the side
-
-Those will be separate challenges coming up.
-*/
-
 import React from "react"
 
 export default function Main() {
     return (
         <main>
-            <h1 className="main--title">Fun facts about React</h1>
+            <h1 className="main--title">Facts about React</h1>
             <ul className="main--facts">
-                <li>Was primarily released in 2013</li>
+                <li>Is free and open-sorce front-end JavaScript Library</li>
+                <li>Was first released in 29 May 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Bullshit guy</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps building user interfaces</li>
             </ul>
         </main>
     )
